bd113eee637abb8784c184058260cf539f1e11b6
michael payage <mica.payage@gmail.com>
2016-12-27 00:57:28 +0100

    effacement projet départ, rectif intégration JS

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 function toggle(anId)
 {
-	node = document.getElementById('foo');
+	node = document.getElementById('Calque_1');
 	
 		// Contenu visible, le cacher
 		node.style.visibility = "hidden";
 		node.style.height = "0";			// Optionnel libérer l'espace
-
+		
+	node = document.getElementById('Calque_2');
+	
+		node.style.visibility = "visible";
+		node.style.display = "block"
+		node.style.height = "0";
 }
