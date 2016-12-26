09f7d287a16d39bf91eeb2f8c6527c1668ad265a
michael payage <mica.payage@gmail.com>
2016-12-26 19:23:17 +0100

    deux version d'anim

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 function toggle(anId)
 {
-	node = document.getElementById(anId);
+	node = document.getElementById('foo');
 	
 		// Contenu visible, le cacher
 		node.style.visibility = "hidden";
 		node.style.height = "0";			// Optionnel libérer l'espace
-		
+
 }
