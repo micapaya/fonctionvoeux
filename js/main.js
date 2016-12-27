520480b88a1eafa7adb9c676b15bd24f377148f3
michael payage <mica.payage@gmail.com>
2016-12-27 01:59:35 +0100

    ajout fonction défilement js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
+												//fonction défilement
+
+var scrl = " POP School vous Souhaite  ";
+
+function scrlsts() 
+{
+	 	scrl = scrl.substring(1, scrl.length) + scrl.substring(0, 1);
+ 		document.title = scrl;
+ 		setTimeout("scrlsts()", 300);
+}
+
+
+												//fonction disparition first page
+
 function toggle(anId)
 {
 	node = document.getElementById('Calque_1');
 	
-		// Contenu visible, le cacher
+															// Contenu visible, le cacher
 		node.style.visibility = "hidden";
-		node.style.height = "0";			// Optionnel libérer l'espace
+		node.style.height = "0";					// Optionnel libérer l'espace
 		
 	node = document.getElementById('Calque_2');
-	
+															// Contenu invisible, le montrer
 		node.style.visibility = "visible";
 		node.style.display = "block"
 		node.style.height = "0";
 }
+
+
